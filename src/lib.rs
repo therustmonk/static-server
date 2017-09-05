4788e86381b618036c52f190d14780cfc285d3a5
Denis Kolodin <deniskolodin@gmail.com>
2017-09-05 11:00:07 +0300

    Implement OnceProvider

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,9 @@ extern crate hyper;
 use std::io;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use std::collections::HashMap;
 use futures::{Future, Stream, Sink};
-use futures::sync::mpsc::{channel, Sender};
+use futures::sync::{oneshot, mpsc};
 use futures_cpupool::CpuPool;
 use tokio_io::AsyncRead;
 use tokio_io::codec::{FramedRead, Decoder};
@@ -16,13 +17,70 @@ use bytes::BytesMut;
 use hyper::{Chunk, Body, Get, StatusCode};
 use hyper::server::{Http, Service, NewService, Request, Response};
 
+
+type SendFile = Box<AsyncRead + Send>;
+
+enum InMsg {
+    Register(String, SendFile),
+    Find(String, oneshot::Sender<Option<SendFile>>),
+}
+
+pub struct OnceProvider {
+    worker: mpsc::Sender<InMsg>,
+}
+
+impl OnceProvider {
+    pub fn new() -> (Self, Box<Future<Item=(), Error=()>>) {
+        let (tx, rx) = mpsc::channel(10);
+        let provider = OnceProvider {
+            worker: tx,
+        };
+        let map: HashMap<String, SendFile> = HashMap::new();
+        let fut = rx.fold(map, |mut map, msg| {
+            match msg {
+                InMsg::Register(path, file) => {
+                    map.insert(path, file);
+                    Ok(map)
+                },
+                InMsg::Find(path, sender) => {
+                    let opt = map.remove(&path);
+                    sender.send(opt)
+                        .map_err(|_| (/* ignore the sender's fail */))
+                        .map(move |_| map)
+                },
+            }
+        }).map(|_| (/* drop the map */));
+        (provider, Box::new(fut))
+    }
+
+    pub fn register<T: AsyncRead + Send + 'static>(&self, path: &str, reader: T) {
+        let msg = InMsg::Register(path.to_owned(), Box::new(reader));
+        self.worker.clone().send(msg).wait().expect("can't add file to a provider");
+    }
+}
+
+impl Provider for OnceProvider {
+    fn find_file<'a>(&self, path: &'a str) -> FindFuture {
+        let (tx, rx) = oneshot::channel();
+        let msg = InMsg::Find(path.to_owned(), tx);
+        let fut = self.worker.clone().send(msg)
+            .map_err(|_| oneshot::Canceled)
+            .and_then(|_| rx)
+            .map_err(|_| other("provider lost"));
+        Box::new(fut)
+    }
+}
+
+
+type FindFuture = Box<Future<Item=Option<SendFile>, Error=io::Error>>;
+
 pub trait Provider: Send + Sync + 'static {
-    fn find_file<'a>(&self, path: &'a str) -> Option<Box<AsyncRead + Send>>;
+    fn find_file<'a>(&self, path: &'a str) -> FindFuture;
 }
 
 pub struct StaticService<T: Provider> {
     provider: Arc<T>,
-    sender: Sender<Job>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl<T: Provider> Service for StaticService<T> {
@@ -34,20 +92,28 @@ impl<T: Provider> Service for StaticService<T> {
     fn call(&self, req: Request) -> Self::Future {
         if req.method() == &Get {
             let path = req.path();
-            if let Some(reader) = self.provider.find_file(path) {
-                let (tx, body) = Body::pair();
-                let fut = self.sender.clone().send((tx, reader))
-                    .map_err(|_| other("can't send task").into())
-                    .map(|_| {
-                        Response::new().with_body(body)
-                    });
-                Box::new(fut)
-            } else {
-                let fut = futures::future::ok(
-                    Response::new().with_status(StatusCode::NotFound)
-                );
-                Box::new(fut)
-            }
+            let (tx, body) = Body::pair();
+            let finder_fut = self.provider.find_file(path);
+            let sender = self.sender.clone();
+            let fut = finder_fut.map_err(hyper::Error::from).and_then(move |reader| {
+                if let Some(reader) = reader {
+                    let send_fut = sender.send((tx, reader));
+                    let fut = send_fut
+                        .map_err(|_| other("can't send task").into())
+                        .map(|_| {
+                            Response::new().with_body(body)
+                        });
+                    let fut: Self::Future = Box::new(fut);
+                    fut
+                } else {
+                    let fut = futures::future::ok(
+                        Response::new().with_status(StatusCode::NotFound)
+                    );
+                    let fut: Self::Future = Box::new(fut);
+                    fut
+                }
+            });
+            Box::new(fut)
         } else {
             let fut = futures::future::ok(
                 Response::new().with_status(StatusCode::MethodNotAllowed)
@@ -57,11 +123,11 @@ impl<T: Provider> Service for StaticService<T> {
     }
 }
 
-type Job = (Sender<hyper::Result<Chunk>>, Box<AsyncRead + Send>);
+type Job = (mpsc::Sender<hyper::Result<Chunk>>, SendFile);
 
 pub struct StaticNewService<T: Provider> {
     provider: Arc<T>,
-    sender: Sender<Job>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl<T: Provider> NewService for StaticNewService<T> {
@@ -80,7 +146,7 @@ impl<T: Provider> NewService for StaticNewService<T> {
 
 pub fn serve<T: Provider>(addr: &SocketAddr, provider: T) {
     let pool = CpuPool::new_num_cpus();
-    let (tx, rx) = channel(10);
+    let (tx, rx) = mpsc::channel(10);
     let new_service = StaticNewService {
         provider: Arc::new(provider),
         sender: tx,
