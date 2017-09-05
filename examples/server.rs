b31d50f0a24c94a1f96bd30f1e3982265ea6b521
Denis Kolodin <deniskolodin@gmail.com>
2017-09-05 23:02:06 +0300

    Rewrite the server with spawning routines inside hyper

--- examples/server.rs
+++ examples/server.rs
@@ -1,14 +1,13 @@
 extern crate static_server;
-extern crate tokio_file_unix;
 
-use static_server::{serve, OnceProvider};
+use std::fs::File;
+use static_server::{serve};
 
 fn main() {
-    let (provider, runner) = OnceProvider::new();
-    let stdin = std::io::stdin();
-    let file = tokio_file_unix::StdFile(stdin.lock());
-    let file = tokio_file_unix::File::new_nb(file).unwrap();
-    provider.register("/myfile", file);
     let addr = "127.0.0.1:8080".parse().unwrap();
-    serve(&addr, provider, runner);
+    let (handle, registrator) = serve(addr);
+    let file = File::open("Cargo.toml").unwrap();
+    registrator.register("/example", file);
+    println!("Started!");
+    handle.join().unwrap().unwrap();
 }
